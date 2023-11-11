e0651d09ebe221ceacc98e743303d244690a855a
Andywrites <54480517+Andywrites@users.noreply.github.com>
2023-11-11 05:23:18 +0100

    Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -19,7 +19,7 @@ contract Ownable {
    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
    * account.
    */
-  constructor() internal {
+  constructor() {
     _owner = msg.sender;
     emit OwnershipTransferred(address(0), _owner);
   }
@@ -35,7 +35,7 @@ contract Ownable {
    * @dev Throws if called by any account other than the owner.
    */
   modifier onlyOwner() {
-    require(isOwner());
+    require(isOwner(), "Caller is not the owner");
     _;
   }
 
@@ -70,7 +70,7 @@ contract Ownable {
    * @param newOwner The address to transfer ownership to.
    */
   function _transferOwnership(address newOwner) internal {
-    require(newOwner != address(0));
+    require(newOwner != address(0), "New owner cannot be the zero address");
     emit OwnershipTransferred(_owner, newOwner);
     _owner = newOwner;
   }
